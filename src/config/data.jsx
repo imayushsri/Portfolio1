23c6fa0a5778128c6b09b5a28379827e7623e95f
imayushsri <ayushsri9666@gmail.com>
2025-07-03 01:16:14 +0530

    first

---
+++ src/config/data.jsx
@@ -0,0 +1,67 @@
+const personalData = {
+    // Home Section
+    name: "Ayush Srivastava",
+    title: ["Full Stack Developer", "Web Designer", "UI/UX Designer"],
+    homeImg: "images/dp.png",
+    email: "ayush@example.com",
+    linkedIn: "https://www.linkedin.com/in/imayushsri/",
+    github: "https://github.com/imayushsri",
+    twitter: "https://twitter.com/imayush_sri",
+    instagram: "https://www.instagram.com/imayushsri/",
+    mail: "mailto:ayush@example.com",
+
+    // About Section
+    aboutImg: "images/about.png",
+    aboutTitle: "Full Stack Developer",
+    aboutText: "Hi, I'm Ayush, a frontend developer based in India. I have a passion for web development and I'm currently learning React, HTML, CSS, and JavaScript. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt laudantium quibusdam voluptate doloribus, hic asperiores at, consequatur assumenda aut porro voluptatibus aspernatur, voluptatem itaque corrupti! Exercitationem doloribus ullam totam? Voluptate! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas iusto porro pariatur amet. Ut aperiam perferendis veritatis recusandae, consectetur sunt fuga hic similique accusantium, quae dicta aliquid adipisci minus fugiat?",
+
+    // Services Section
+    service1Title: "Web Development",
+    service1Text: "Experienced in building responsive and user-friendly websites using HTML, CSS, and JavaScript.",
+    service2Title: "Graphic Design",
+    service2Text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae pariatur, cum explicabo.",
+    service3Title: "Android Development",
+    service3Text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae pariatur, cum explicabo.",
+
+    // Education Section
+    edu1Year: "2022 - 2025",
+    edu1Course: "Diploma",
+    edu1Stream: "Computer Science",
+    edu1College: "College Name",
+    edu1Grade: "A+",
+
+    edu2Year: "2022 - 2025",
+    edu2Course: "Diploma",
+    edu2Stream: "Computer Science",
+    edu2College: "College Name",
+    edu2Grade: "A+",
+
+    edu3Year: "2022 - 2025",
+    edu3Course: "Diploma",
+    edu3Stream: "Computer Science",
+    edu3College: "College Name",
+    edu3Grade: "A+",
+
+    // Portfolio Section
+    project1Title: "Project 1",
+    project1Img: "images/project1.jpeg",
+    project1Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae pariatur, cum explicabo.",
+    project2Title: "Project 2",
+    project2Img: "images/project2.png",
+    project2Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae pariatur, cum explicabo.",
+    project3Title: "Project 3",
+    project3Img: "images/project3.png",
+    project3Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae pariatur, cum explicabo.",
+    project4Title: "Project 4",
+    project4Img: "images/project4.webp",
+    project4Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae pariatur, cum explicabo.",
+    project5Title: "Project 5",
+    project5Img: "images/project5.jpg",
+    project5Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae pariatur, cum explicabo.",
+    project6Title: "Project 6",
+    project6Img: "images/project6.webp",
+    project6Text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae pariatur, cum explicabo.",
+
+};
+
+export default personalData;
